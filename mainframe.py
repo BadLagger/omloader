0470cbe8427cc70ba6358467a53cbca108fd375b
BadLagger <lanynamer1985@gmail.com>
2022-02-20 21:43:52 +0300

    Added logic for edits and button

--- mainframe.py
+++ mainframe.py
@@ -2,19 +2,51 @@ import tkinter as tk
 from tkinter import filedialog as fd
 from tkinter import ttk
 import json
+from os import path
 
 class PathChooseElement:
-    def __init__(self, title="Some text", row_num=0):
+    def __init__(self, title="Some text", row_num=0, callback=None):
+        self.__row_id = row_num
         self.__lbl = tk.Label(text=title);
         self.__lbl.grid(row=row_num, column=0, sticky=tk.W, padx=5, pady=3)
         self.__path_var = tk.StringVar()
+        self.__path_var.trace_add('write', lambda *args: self.__validate_entry())
         self.__entry = tk.Entry(width=75, textvariable=self.__path_var)
         self.__entry.grid(row=row_num, column=1, columnspan=4, sticky=tk.W, pady=3)
         self.__btn = tk.Button(text='...', command=self.__dialog_cmd)
         self.__btn.grid(row=row_num, column=5, sticky=tk.W, padx=5, pady=3)
+        self.__callback = callback
+
+    def get_path(self):
+        return self.__path_var.get()
+
+    def set_path(self, p):
+        if path.isfile(p):
+            revert_slash_list = p.split('\\')
+            if len(revert_slash_list) > 1:
+                revert_slash = '/'.join(revert_slash_list)
+                self.__path_var.set(revert_slash)
+            else:
+                self.__path_var.set(p)
+
+    def __validate_entry(self):
+        #print('Callback')
+        _path = self.__path_var.get()
+        if path.isfile(_path):
+            revert_slash_list = _path.split('\\')
+            #print(revert_slash_list )
+            if len(revert_slash_list) > 1:
+                revert_slash = '/'.join(revert_slash_list)
+                self.__path_var.set(revert_slash)
+            if self.__callback:
+                self.__callback(True, self.__row_id)
+        else:
+            if self.__callback:
+                self.__callback(False, self.__row_id)
 
     def __dialog_cmd(self):
         last_path = self.__path_var.get()
+        print(last_path)
         if len(last_path):
             last_path_dir = '/'.join(last_path.split('/')[0:-1])
             init_dir = last_path_dir
@@ -27,7 +59,7 @@ class PathChooseElement:
 
 import serial.tools.list_ports
 class PortElement:
-    def __init__(self, title="Port", row_num=0):
+    def __init__(self, title="Port", row_num=0, fw_update_cmd=None):
         self.__lbl = tk.Label(text=title);
         self.__lbl.grid(row=row_num, column=0, sticky=tk.W, padx=5, pady=3)
         self.__list = ttk.Combobox(state=tk.DISABLED)
@@ -39,8 +71,14 @@ class PortElement:
         self.__erase_var = tk.IntVar()
         self.__check = tk.Checkbutton(text="Со стиранием", variable=self.__erase_var, command=self.__erase_cmd)
         self.__check.grid(row=row_num, column=3, sticky="we", pady=3)
-        self.__btn = tk.Button(text='Прошить')
+        self.__btn = tk.Button(text='Прошить', state=tk.DISABLED, command=fw_update_cmd)
         self.__btn.grid(row=row_num, column=4, columnspan=2, sticky="we", padx=5, pady=3)
+
+    def enable_update(self):
+        self.__btn['state'] = tk.NORMAL
+
+    def disable_update(self):
+        self.__btn['state'] = tk.DISABLED
 
     def __erase_cmd(self):
         if self.__erase_var.get():
@@ -83,17 +121,37 @@ class MainFrame:
         self.__w.resizable(False, False)
         self.__w.protocol('WM_DELETE_WINDOW', self.__on_clossing_event)
         # Path elements
-        self.__uuu = PathChooseElement("Путь к UUU:", 0)
-        self.__loader = PathChooseElement("Путь к загрузчику:", 1)
-        self.__fw = PathChooseElement("Путь к прошивке:", 2)
+        self.__uuu = PathChooseElement("Путь к UUU:", 0, self.__update_from_entry)
+        self.__loader = PathChooseElement("Путь к загрузчику:", 1, self.__update_from_entry)
+        self.__fw = PathChooseElement("Путь к прошивке:", 2, self.__update_from_entry)
+        self.__allow_update = [False, False, False]
         # Port element
-        self.__port = PortElement("Порты:", 3)
+        self.__port = PortElement("Порты:", 3, self.update_fw)
         # ProgressBar
         self.__bar = ProgressBarElement(4, 6)
+        # Initial states
+        if self.__cfg.get('uuu'):
+            self.__uuu.set_path(self.__cfg['uuu'])
+        if self.__cfg.get('loader'):
+            self.__loader.set_path(self.__cfg['loader'])
+        if self.__cfg.get('fw'):
+            self.__fw.set_path(self.__cfg['fw'])
 
 
     def go(self):
         self.__w.mainloop()
+
+    def update_fw(self):
+        print('Try update')
+        pass
+
+    def __update_from_entry(self, state, id):
+        #print('Entry external callback: %s %d' % (state, id))
+        self.__allow_update[id] = state
+        if self.__allow_update[0] and self.__allow_update[1] and self.__allow_update[2]:
+            self.__port.enable_update()
+        else:
+            self.__port.disable_update()
 
     def __open_cfg(self):
         cfg = {}
@@ -109,6 +167,13 @@ class MainFrame:
         return cfg
 
     def __save_cfg(self):
+        pathes = [self.__uuu.get_path(), self.__loader.get_path(), self.__fw.get_path()]
+        if path.isfile(pathes[0]):
+            self.__cfg['uuu'] = pathes[0]
+        if path.isfile(pathes[1]):
+            self.__cfg['loader'] = pathes[1]
+        if path.isfile(pathes[2]):
+            self.__cfg['fw'] = pathes[2]
         with open('oml.cfg', 'w') as f:
             json.dump(self.__cfg, f)
 
